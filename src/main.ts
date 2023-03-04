5b9a5780647365f260838d2c4c794ee2c01f83d8
Brenden Matthews <github@brenden.brndn.io>
2023-03-04 14:53:04 -0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 const fetchRustup = async () => {
+  core.startGroup('Install rustup')
   const rustupInstaller =
     process.platform === 'win32'
       ? await downloadTool('https://win.rustup.rs')
@@ -13,12 +14,11 @@ const fetchRustup = async () => {
 
   await exec(rustupInstaller, ['--default-toolchain', 'none', '-y'])
 
-  core.info(process.env.HOME)
-
   core.addPath(path.join(process.env.HOME ?? process.cwd(), '.cargo', 'bin'))
 
   await exec('rustup', ['show'])
   await exec('cargo', ['--version'])
+  core.endGroup()
 }
 
 async function run(): Promise<void> {
