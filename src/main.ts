54aa08f01c440f8d7f8dcd197234d75ffc176fd4
Brenden Matthews <github@brenden.brndn.io>
2023-03-04 14:37:55 -0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-import { addPath, info, setFailed } from '@actions/core'
-import { exec } from '@actions/exec'
-import { downloadTool } from '@actions/tool-cache'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as tc from '@actions/tool-cache'
 import { promises as fs } from 'fs'
 import path from 'path'
 
 const fetchRustup = async () => {
   const rustupInstaller =
     process.platform === 'win32'
-      ? await downloadTool('https://win.rustup.rs')
-      : await downloadTool('https://sh.rustup.rs')
+      ? await tc.downloadTool('https://win.rustup.rs')
+      : await tc.downloadTool('https://sh.rustup.rs')
   await fs.chmod(rustupInstaller, 0o755)
 
-  await exec(rustupInstaller, ['--default-toolchain', 'none', '-y'])
+  await exec.exec(rustupInstaller, ['--default-toolchain', 'none', '-y'])
 
-  await exec('rustup show')
-  await exec('cargo --version')
+  await exec.exec('rustup show')
+  await exec.exec('cargo --version')
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  addPath(path.join(process.env.HOME!, '.cargo', 'bin'))
+  core.addPath(path.join(process.env.HOME!, '.cargo', 'bin'))
 }
 
 async function run(): Promise<void> {
   try {
-    info('hello!!!!!')
+    core.info('hello!!!!!')
     await fetchRustup()
   } catch (error) {
-    if (error instanceof Error) setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
